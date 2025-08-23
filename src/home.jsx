bb3dd7150d7c9405deb496f7b1465cf951ed2d1b
aibaljose <aibaljosej@gmail.com>
2025-08-23 09:46:24 +0530

    added login

--- src/home.jsx
+++ src/home.jsx
@@ -2,15 +2,21 @@ import React, { useState } from 'react';
 import "./home.css";
 import "./modal.css";
 import maveli from "./assets/Maveli-12.png";
+import { useNavigate } from "react-router-dom";
 
 
 const Home = () => {
+    const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/login");
+  };
+
     return (
         <div>
             <div className="container">
                 <h1>µ-onam</h1>
                 <p>the real hunt begin</p>
-                <div className="btn" onClick={() => setShowModal(true)}>Start Hunt</div>
+                <div className="btn" onClick={handleClick}>Start Hunt</div>
             </div>
             <div className="bottom-img-container">
                 <img src={maveli} alt="Bottom Image" />
